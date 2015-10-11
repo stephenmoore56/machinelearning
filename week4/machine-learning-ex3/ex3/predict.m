8dc58980d57d0f345f97915a77c00ac3a5c131ca
Stephen Moore <stephenmoore56@msn.com>
2015-10-10 19:43:16 -0500

    Finished assignment

--- week4/machine-learning-ex3/ex3/predict.m
+++ week4/machine-learning-ex3/ex3/predict.m
@@ -17,13 +17,20 @@ p = zeros(size(X, 1), 1);
 %
 % Hint: The max function might come in useful. In particular, the max
 %       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
+%%       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% add bias unit to X and calculate sigmoid
+X = [ones(m, 1) X];
+a2 = sigmoid(X * Theta1');
 
+% add bias unit to a2 and calculate sigmoid
+a2 = [ones(m, 1) a2];
+Theta3 = sigmoid(a2 * Theta2');
 
-
+% get max in each row
+[temp, p] = max(Theta3, [], 2);
 
 
 
